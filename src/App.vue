af9ea8a47adf27a68f817d1baeff4da0794fef1b
jhon134 <12063482+jhon134@user.noreply.gitee.com>
2024-03-11 13:20:32 +0800

    'init'

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="a">
+    <div class="b">
+    </div>
+  </div>
+  <button @click="connect()">连接</button>
+  <video ref="vid"></video>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import {onMounted,ref} from "vue"
+import mqtt from "mqtt"
+let broker = "ws://broker.emqx.io:8083/mqtt"
+var client;
+const vid = ref({})
+onMounted(()=>{
+  navigator.mediaDevices.getUserMedia({
+    video:true
+  }).then((stream)=>{
+    vid.value.srcObject=stream;
+    vid.value.play();
+  }).finally(()=>{
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  })
+})
+
+
+function connect(){
+  client = mqtt.connect(broker,{
+    clientId:Math.random().toFixed(32).toString().substring(2),
+    clean:true,
+    keepalive:30,
+    
+  })
+  client.on("error",(err)=>{
+    alert("连接失败！"+JSON.stringify(err));
+  })
+  client.on("connect",(err)=>{
+    alert("连接成功！"+JSON.stringify(err))
+  })
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.a{
+  background-color: blue;
+  padding: 10px;
+  .b{
+    background-color:  red;
+    width: 100px;
+    height: 100px;
+  }
 }
 </style>
