b19b16fd09b8bbf095e4b99a25eca99b3dae5fcf
jhon134 <12063482+jhon134@user.noreply.gitee.com>
2024-03-12 18:17:07 +0800

    'element-plus集成'

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,11 @@
 <template>
-  <div class="a">
-    <div class="b">
-    </div>
-  </div>
-  <button @click="connect()">连接</button>
-  <video ref="vid"></video>
+  <el-button type="info" >你好</el-button>
 </template>
 
 <script setup>
-import {onMounted,ref} from "vue"
-import mqtt from "mqtt"
-let broker = "ws://broker.emqx.io:8083/mqtt"
-var client;
-const vid = ref({})
-onMounted(()=>{
-  navigator.mediaDevices.getUserMedia({
-    video:true
-  }).then((stream)=>{
-    vid.value.srcObject=stream;
-    vid.value.play();
-  }).finally(()=>{
 
-  })
-})
-
-
-function connect(){
-  client = mqtt.connect(broker,{
-    clientId:Math.random().toFixed(32).toString().substring(2),
-    clean:true,
-    keepalive:30,
-    
-  })
-  client.on("error",(err)=>{
-    alert("连接失败！"+JSON.stringify(err));
-  })
-  client.on("connect",(err)=>{
-    alert("连接成功！"+JSON.stringify(err))
-  })
-}
 </script>
 
 <style lang="scss">
-.a{
-  background-color: blue;
-  padding: 10px;
-  .b{
-    background-color:  red;
-    width: 100px;
-    height: 100px;
-  }
-}
+
 </style>
---
+++ src/js/mqtt/index.js
@@ -0,0 +1,22 @@
+import mqtt from "mqtt"
+
+function connect(broker:string){
+    const client = mqtt.connect(broker,{
+        clientId:Math.random().toFixed(32).toString().substring(2),
+        clean:true,
+        keepalive:30,
+        
+    })
+    client.on("error",(err)=>{
+        alert("连接失败！"+JSON.stringify(err));
+        return new Promise((resolve,reject)=>{
+            reject(err);
+        })
+    })
+    client.on("connect",(err)=>{
+        alert("连接成功！"+JSON.stringify(err));
+        return new Promise((resolve,reject)=>{
+            resolve(client);
+        })
+    })
+}
