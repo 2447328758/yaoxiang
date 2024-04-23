df34ae478ee590836a3b1314a8e7feeabc7beceb
jhon134 <12063482+jhon134@user.noreply.gitee.com>
2024-04-23 19:46:49 +0800

    设置药品数量添加标签，设置响应

--- src/pages/UserProfile.vue
+++ src/pages/UserProfile.vue
@@ -19,37 +19,42 @@
         </el-row>
         <el-divider></el-divider>
         <el-row justify='space-around' align="middle">
-            <el-input-number v-model="a" placeholder="a药数量" max="9" min="0" size="default" value-on-clear="min"></el-input-number>
+            <el-text>药品A</el-text>
+            <el-input-number v-model="form.a" placeholder="a药数量" max="9" min="0" size="small" value-on-clear="min"></el-input-number>
             <el-divider direction='vertical'></el-divider>
-            <el-input-number v-model="b" placeholder="b药数量" max="9" min="0" size="default" value-on-clear="min"></el-input-number>
+            <el-text>药品B</el-text>
+            <el-input-number v-model="form.b" placeholder="b药数量" max="9" min="0" size="small" value-on-clear="min"></el-input-number>
         </el-row>
         <el-row justify='space-around' align="middle">
-            <el-input-number v-model="c" placeholder="c药数量" max="9" min="0" size="default" value-on-clear="min"></el-input-number>
+            <el-text>药品C</el-text>
+            <el-input-number v-model="form.c" placeholder="c药数量" max="9" min="0" size="small" value-on-clear="min"></el-input-number>
             <el-divider direction='vertical'></el-divider>
-            <el-input-number v-model="d" placeholder="d药数量" max="9" min="0" size="default" value-on-clear="min"></el-input-number>
+            <el-text>药品D</el-text>
+            <el-input-number v-model="form.d" placeholder="d药数量" max="9" min="0" size="small" value-on-clear="min"></el-input-number>
         </el-row>
         <el-row justify="end">
             <el-button style="margin:5px;" type="primary" @click="setDis()">设置</el-button>
         </el-row>
+
+     
         <!-- <el-button @click="send()">test send(发送uint8array(12344))</el-button> -->
 
     </div>
 </template>
 <script setup>
-import {ref} from 'vue'
+import {reactive,ref} from 'vue'
 import { useStore } from 'vuex';
 import { sendCmd } from '@/js/mqtt';
+import { ElMessage } from 'element-plus';
 const input1 = ref();
 const input2 = ref();
 const input3 = ref();
-const a = ref();
-const b = ref();
-const c = ref();
-const d = ref();
-a.value=0;
-b.value=0;
-c.value=0;
-d.value=0;
+const form =reactive({
+    a:0,
+    b:0,
+    c:0,
+    d:0
+}) 
 const store=useStore();
 import {to2bit} from '../js/utils/nf'
 
@@ -66,13 +71,22 @@ function setEndt(ord){
     //let a = new Uint8Array(2);
     //a[0]=input.getHours();a[1]=input.getMinutes();
     sendCmd(prefix+`${to2bit(input.getHours())}${to2bit(input.getMinutes())}`);
+    ElMessage({
+                    message: "设置成功！",
+                    type: 'success',
+                    })
 }
 /* eslint-disable */
 
 
 function setDis(){
     // console.log("dis"+`${a.value}${b.value}${c.value}${d.value}`)
-    sendCmd("dis"+`${a.value}${b.value}${c.value}${d.value}`);
+    // console.log(form)
+    sendCmd("dis"+`${form.a}${form.b}${form.c}${form.d}`);
+    ElMessage({
+                    message: "设置成功！",
+                    type: 'success',
+                    })
 }
 
 
